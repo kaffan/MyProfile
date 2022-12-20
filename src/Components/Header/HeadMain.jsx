e0eed7ecf822ef4694621c717193763eff9f7c49
kaffan <kaffan022@gmail.com>
2022-12-20 23:20:10 +0530

    Latest commit

--- src/Components/Header/HeadMain.jsx
+++ src/Components/Header/HeadMain.jsx
@@ -8,31 +8,33 @@ import ScrollToAbout from './ScrollToAbout';
 const HeadMain = () =>{
     return(
         <>
-          <Grid container direction="column" rowSpacing={3}
+          <Grid container direction="column" rowSpacing={8}
             sx={{
               backgroundImage:"linear-gradient(to right,rgba(221, 181, 181,0.5),rgba(0, 219, 175,1),rgba(12, 131, 137,1))",
-              height:"100%",
               margin:"0"
             }} 
           >
-            <Grid item><Navbar /></Grid>
+            <Grid item 
+             sx={{flexGrow:"2"}}
+            ><Navbar /></Grid>
             <Grid item
-            sx={{flexGrow:"5",alignItems:"center"}}
+            sx={{flexGrow:"7",alignItems:"center",flexWrap:"wrap"}}
             >
                 <Grid container spacing={0}
                  sx={{height:"100%",alignItems:"center",justifyContent:"center"}}
                  >
-                    <Grid item xs={5} sx={{textAlign:"left",padding:"0 2em"}}>
+                    <Grid item md={5} xs={8} sx={{textAlign:"left",padding:"0 2em"}}>
                       <MyImage></MyImage>
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item md={6} xs={8}>
                         <TitleName></TitleName>
                     </Grid>
                 </Grid>
             </Grid>
             <Grid item
             sx={{
-              alignItems:"center"
+              alignItems:"center",
+              flexGrow:"2"
             }}>
               <ScrollToAbout></ScrollToAbout>
             </Grid>
