e0f8b017b77102f371606a7563a6889b28823177
kaffan <kaffan022@gmail.com>
2022-12-22 12:46:22 +0530

    latest commit

--- src/Components/Header/MyImage.jsx
+++ src/Components/Header/MyImage.jsx
@@ -14,7 +14,7 @@ const MyImage = () =>{
    ` 
    const theme = useTheme();
    const match2 = useMediaQuery(theme.breakpoints.down('md'));
-   const match3 = useMediaQuery(`(max-width:600px)`);
+   const match3 = useMediaQuery(`(max-width:700px)`);
     return(
         <>
             <img src="/profileImg.jpg"
