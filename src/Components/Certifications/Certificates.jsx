0064e594dbd36c80d8583fbde5932be4707787fd
kaffan <kaffan022@gmail.com>
2023-02-14 22:57:11 +0530

    latest commit on 14th feb at 11  PM

---
+++ src/Components/Certifications/Certificates.jsx
@@ -0,0 +1,14 @@
+import { Cheerio } from "cheerio";
+import { Fragment } from "react";
+
+const Certificates = () =>{
+    // Cheerio.fromURL('https://www.linkedin.com/in/affan-khan-6626b9195/')
+    // .then(())
+    return(
+        <Fragment>
+
+        </Fragment>
+    );
+}
+
+export default Certificates;
